70add6b4acce561729a8d200e2fd290a331a45aa
Taylor Sloan <taylor.sloan2@gmail.com>
2022-01-14 16:59:00 -0800

    Added deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const NAME = "Galaget";
+const SYMBOL = "GLGT";
+const BASE_URI = "ipfs://FAKE_CID/";
+
+async function main() {
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
+
+    // We get the contract to deploy
+    const Galaget = await hre.ethers.getContractFactory("Galaget");
+    const galaget = await Galaget.deploy(NAME, SYMBOL, BASE_URI);
+    await galaget.deployed();
+    console.log("Galaget deployed to:", galaget.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
